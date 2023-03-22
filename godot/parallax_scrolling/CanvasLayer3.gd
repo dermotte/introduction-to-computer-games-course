29548fc25b01f46e4762129d6511c2f60653a49a
Mathias Lux <mathias@juggle.at>
2023-03-22 13:39:04 +0100

    parallax scrolling example

---
+++ godot/parallax_scrolling/CanvasLayer3.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+var _offset = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var position = get_offset()
+	position.x = _offset
+	set_offset(position)
+	_offset -= (20 * delta) 
+	if _offset <= -1024:
+		_offset += 1024
